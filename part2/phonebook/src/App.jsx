3209dd8d17cc8c30c7b792805b4166421bdfb692
Zizhan Zhou <zizzhou@lutron.com>
2023-09-20 16:32:34 -0500

    exercise 2.6-2.9

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,67 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ]) 
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const personObject = {name: newName, number: newNumber}
+    if (persons.some(person => person.name === personObject.name)) {
+      alert(`${personObject.name} is already added to phonebook`)
+    }
+    else {
+      setPersons(persons.concat(personObject))
+    }
+    setNewName('')
+  }
+
+  const handlePersonChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    console.log(event.target.value)
+    setNewNumber(event.target.value)
+  }
+
+  const filterPersons = (event) => {
+    console.log(event.target.value)
+    setPersons(persons.filter(person => person.name.toLowerCase().includes(event.target.value.toLowerCase())))
+  }
+
+
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <div>
+        filter shown with <input onChange={filterPersons} />
+      </div>
+      <h2>add a new</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input  value={newName} onChange={handlePersonChange}/>
+        </div>
+        <div>
+          number <input  value={newNumber} onChange={handleNumberChange}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map(person => <li key={person.name.length+Math.random()}>{person.name}  {person.number}</li>)}
+      </ul>
+    </div>
+  )
+}
+
+export default App
