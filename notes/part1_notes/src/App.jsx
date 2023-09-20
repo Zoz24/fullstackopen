65fa153075bc1a4c97c0416b66ce73448affb8a7
Zizhan Zhou <zizzhou@lutron.com>
2023-09-20 15:20:17 -0500

    notes update

--- notes/part1_notes/src/App.jsx
+++ notes/part1_notes/src/App.jsx
@@ -1,36 +1,28 @@
-import { useState } from "react";
+import { useState } from 'react'
 
-const History = (props) => {
-  if (props.allClicks.length === 0) {
-    return <div>the app is used by pressing the buttons</div>;
-  }
-  return <div>button press history: {props.allClicks.join(" ")}</div>;
-};
+const Display = ({ counter }) => <div>{counter}</div>
+
+const Button = ({ handleClick, text }) => <button onClick={handleClick}>{text}</button>
+
 
 const App = () => {
-  const [left, setLeft] = useState(0);
-  const [right, setRight] = useState(0);
 
-  const [allClicks, setAll] = useState([]);
+  const [ counter, setCounter ] = useState(0)
 
-  const handleLeftClick = () => {
-    setAll(allClicks.concat("L"));
-    setLeft(left + 1);
-  };
 
-  const handleRightClick = () => {
-    setAll(allClicks.concat("R"));
-    setRight(right + 1);
-  };
+  const increaseByOne = () => setCounter(counter + 1)
+  const decreaseByOne = () => setCounter(counter - 1)
+  const setToZero = () => setCounter(0)
+
+
   return (
-    <div>
-      {left}
-      <button onClick={handleLeftClick}>left</button>
-      <button onClick={handleRightClick}>right</button>
-      {right}
-      <History allClicks={allClicks} />
-    </div>
-  );
-};
+    <>
+    <Display counter={counter}/>
+    <Button handleClick={increaseByOne} text='plus'/>
+    <Button handleClick={setToZero} text='zero'/>
+    <Button handleClick={decreaseByOne} text='minus'/>
+    </>
+  )
+}
 
-export default App;
+export default App
