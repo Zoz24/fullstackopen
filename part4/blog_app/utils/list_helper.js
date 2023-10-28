6b1b00a2503ae05c700bd2f34702b38c6de283fb
zoz24 <zizhanzhou24@gmail.com>
2023-10-28 15:08:16 -0500

    done up to 4.5

--- part4/blog_app/utils/list_helper.js
+++ part4/blog_app/utils/list_helper.js
@@ -2,6 +2,18 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  return blogs.reduce((sum, blog) => sum + blog.likes, 0)
+}
+
+const favoriteBlog = (blogs) => {
+  return blogs.reduce((favorite, blog) => {
+    return favorite.likes > blog.likes ? favorite : blog
+  }, {})
+}
+
 module.exports = {
   dummy,
+  totalLikes,
+  favoriteBlog,
 }
