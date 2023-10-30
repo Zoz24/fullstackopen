9f65ea8295cf5c40aef8dad7d980474cab31f42a
zoz24 <zizhanzhou24@gmail.com>
2023-10-30 17:57:15 -0500

    update

--- part4/blog_app/controllers/blogs.js
+++ part4/blog_app/controllers/blogs.js
@@ -1,4 +1,5 @@
 const blogRouter = require('express').Router()
+const blog = require('../models/blog')
 const Blog = require('../models/blog')
 
 blogRouter.get('/', (request, response) => {
@@ -13,6 +14,26 @@ blogRouter.post('/', (request, response) => {
   blog.save().then((result) => {
     response.status(201).json(result)
   })
+})
+
+blogRouter.delete('/:id', (request, response) => {
+  Blog.findByIdAndRemove(request.params.id).then((result) => {
+    response.status(204).end()
+  })
+})
+
+blogRouter.put('/:id', async (request, response) => {
+  const body = request.body
+  const blog = {
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+  }
+  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, {
+    new: true,
+  })
+  response.json(updatedBlog)
 })
 
 module.exports = blogRouter
