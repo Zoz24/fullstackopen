889d0935c406df4d17a22c476aea795d67fd6e44
zoz24 <zizhanzhou24@gmail.com>
2023-11-04 23:47:23 -0500

    4.18

--- part4/blog_app/app.js
+++ part4/blog_app/app.js
@@ -6,6 +6,7 @@ const config = require('./utils/config')
 const logger = require('./utils/logger')
 const blogRouter = require('./controllers/blogs')
 const userRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 mongoose.set('strictQuery', false)
 
@@ -25,5 +26,6 @@ app.use(express.json())
 
 app.use('/api/blogs', blogRouter)
 app.use('/api/users', userRouter)
+app.use('/api/login', loginRouter)
 
 module.exports = app
