364586436b83eb8746758ecaebcae8dcd2bc59f4
zoz24 <zizhanzhou24@gmail.com>
2023-11-05 13:38:25 -0600

    4.20

--- part4/blog_app/app.js
+++ part4/blog_app/app.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
+const middleware = require('./utils/middleware')
 const blogRouter = require('./controllers/blogs')
 const userRouter = require('./controllers/users')
 const loginRouter = require('./controllers/login')
@@ -24,8 +25,13 @@ mongoose
 app.use(cors())
 app.use(express.json())
 
+app.use(middleware.tokenExtractor)
+
 app.use('/api/blogs', blogRouter)
 app.use('/api/users', userRouter)
 app.use('/api/login', loginRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
